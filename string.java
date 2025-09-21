8f82af9611dd4ce3dcf1c91d4625babba3278c97
altedante4-cyber <altedante4@gmail.com>
2025-09-21 18:49:07 +0200

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string {
+
+    public static  void main (String [] args){
+
+        //Cadenas de texto
+
+        String name = "michael";
+        var surname = new String(name);  // estos es el constructo  del string
+
+    }
+}
