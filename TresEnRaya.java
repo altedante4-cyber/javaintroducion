38a49aba720d35c87220d9bb23e0c9abcdec36f0
michael aguilar <altedante4@gmail.com>
2025-11-23 22:30:46 +0100

    juego

--- TresEnRaya.java
+++ TresEnRaya.java
@@ -3,263 +3,165 @@ package juego;
 import java.util.Scanner;
 
 public class TresEnRaya {
+    static Scanner entrada = new Scanner(System.in);
+    static char[] tablero = new char[9];
+    static int ganadasJugador1 = 0;
+    static int ganadasJugador2 = 0;
+    static int erroresJugador1 = 0;
+    static int erroresJugador2 = 0;
+    static int empates = 0;
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        char[] tablero = new char[9];
-        
-        
-        // iniciamos la partida con un bucle do while para validar la entrada si el usuario introduce el 1 entonces entramos en el bucle else if 
+        dibujaTablero(); // iniciar tablero vacío
+
         do {
-        	
-        	System.out.println("1.Jugar una partida");
-        	System.out.println("2.Mostrar estadícas");
-        	System.out.println("3.Salir");
-            System.out.println("Seleccione una opción ");        
-        	int opcion ;
-        	
-        	opcion = entrada.nextInt();
-        	
-        	if(opcion == 3) {
-        		
-        		break;
-        	}
-        	else if (opcion == 1 ) {
-        			System.out.println("-----------");
-        		    System.out.println("| 1 | 2 | 3| ");
-        	        System.out.println("-----------");
-        	        System.out.println("| 4 | 5 | 6| ");
-        	        System.out.println("-----------");
-        	        System.out.println("| 7 | 8 | 9|");
-        	        System.out.println("-----------");
-        	        boolean turnoX = true;
-                 
-             
-                int contador = 0 ;
-                int posicion ;  
-                
-                
-                 while (contador <= 9 ) {
+            mostrartable();
+            int opcion = entrada.nextInt();
+            int jugador = 1; // 1 = Jugador 1 (X), 2 = Jugador 2 (O)
 
-                 	
-                 	
-                          // pedimos al usuario que le toque la posicion
-                    
-                	 
-                	 
-                	 while (true) {
-                		 
-                		  
-                   	  System.out.print("Jugador " + (turnoX ? "X" : "O") + ", elige posición (1-9): ");
+            if (opcion == 3) {
+                break;
+            } else if (opcion == 1) {
+                dibujaTablero(); // tablero vacío al iniciar partida
+                boolean repetir = true;
+                int contador = 0;
 
-                        posicion = entrada.nextInt()- 1 ;
-                       
-                     
-                         if(posicion >= 1 || posicion <= 9) {
-                       	 
-                              break;
-                         	 
-                             }else {
-                            	 
-                            	  
-                               System.out.println("posicion invalida ");
-                         }
-                       
-                	 }
-                        	
+                while (repetir) {
+                    while (contador < 9) {
+                        int posicion;
 
-                          
+                        // Pedir posición válida
+                        do {
+                            System.out.println("Jugador " + jugador + ", elige posición (1-9): ");
+                            posicion = entrada.nextInt() - 1;
 
-                	 System.out.println("-------------");
-                     for (int i = 0; i < 9; i++) {
-                         
-                     	System.out.print("| " + tablero[i] + " ");
-                         if ((i + 1) % 3 == 0) {   /// mostramos el tablero cada tres lineas 
-                             System.out.println("|");
-                             System.out.println("-------------");
-                         }
-                     }
-                     
-                   	
-                    	if (tablero[posicion] == ' ' || tablero[posicion] != 'X' && tablero[posicion] !=  'O'){
-                        	
-                    		
-                    		
-                       	 tablero[posicion] = turnoX ? 'X' : 'O';	
-                    	   
-                     	// cambiamos de turno 
-                        	
-                    		 turnoX = !turnoX ; 
-                 	 
-                       		
-                       	}
-                       
-                          
-                        
-                     
-                        	
-                         
-                         
-                  		
-                        	   
-                        	
-                    	  
-            
-                    	
-                    	// esto lo utilizamos para que cuando el usuario introdusca el numero el turno que le toque ponga su signo 
-                        
-                          
-                     
-                    	// tablero[posicion] = turnoX ? 'X' : 'O';	
-                  	   
-                        	// cambiamos de turno 
-                           	
-                       	//	 turnoX = !turnoX ; 
-                    	 
-                     
-                      // para que no vuelva a seleccionar la misma casilla marcada volvermos a pedir al usuario 
-                          
-           
-                  
-                    	
-                    	
-                    
-                    
-                    	  
-              
-                       
-                        	
-                   		
-                     	
-                        
-                          
-                 		
-                 	      
-                          
-                      	
-  
-                      	// comprobanos si alguien ha ganado 
-                  
+                            if (posicion < 0 || posicion > 8) {
+                                System.out.println("Posición inválida");
+                                if (jugador == 1) erroresJugador1++;
+                                else erroresJugador2++;
+                            }
 
-                        if (tablero[0] == 'X' && tablero[1] == 'X' && tablero[2] == 'X' ) {  // esto es la fila 1  diagonal
-                       	     System.out.println("Jugador X ha ganado"); 
-                       	    break; 
-   
-                       	} else  
-                        if (tablero[3] == 'X' && tablero[4] == 'X' && tablero[5] == 'X') {  // esto es la fila 2 diagonal
-                    	    System.out.println("Jugador X ha ganado"); 
-                        	   break;
-                       	}else  
-                            if (tablero[6] == 'X' && tablero[7] == 'X' && tablero[8] == 'X') {  // esto es la file 3 diagonal
-                        	    System.out.println("Jugador X ha ganado"); 
-                            	   break;
-                           	}
-                            else  
-                                if (tablero[0] == 'X' && tablero[3] == 'X' && tablero[6] == 'X') {  // primera fila de la primera columana  
-                            	    System.out.println("Jugador X ha ganado"); 
-                                	   break;
-                               	}
-                                else  
-                                    if (tablero[1] == 'X' && tablero[4] == 'X' && tablero[7] == 'X') {  // 2 columna 
-                                	    System.out.println("Jugador X ha ganado"); 
-                                    	   break;
-                                   	}else  
-                                        if (tablero[2] == 'X' && tablero[5] == 'X' && tablero[8] == 'X') {  // 3 columna 
-                                     	    System.out.println("Jugador X ha ganado"); 
-                                        	   break;
-                                       	}else  
-                                            if (tablero[0] == 'X' && tablero[4] == 'X' && tablero[8] == 'X') {  // 3 columna 
-                                         	    System.out.println("Jugador X ha ganado"); 
-                                            	   break;
-                                           	}else  
-                                                if (tablero[2] == 'X' && tablero[4] == 'X' && tablero[6] == 'X') {  // 3 columna 
-                                             	    System.out.println("Jugador X ha ganado"); 
-                                                	   break;
-                                               	}else {
-                                               	  if (tablero[0] == 'O' && tablero[1] == 'O' && tablero[2] == 'O') {  // primera fila 
-                                                      System.out.println("Jugador O ha ganado");
-                                                  }else
-                                                  	if (tablero[3] == 'O' && tablero[4] == 'O' && tablero[5] == 'O') {  // segunda fila 
-                                                      System.out.println("Jugador O ha ganado");
-                                                  }else
-                                                  	if (tablero[6] == 'O' && tablero[7] == 'O' && tablero[8] == 'O') {   // tercera fila 
-                                                          System.out.println("Jugador O ha ganado");
-                                                      }
-                                                  	else
-                                                      	if (tablero[0] == 'O' && tablero[3] == 'O' && tablero[6] == 'O') { // primera fila de la pirmera columna
-                                                          System.out.println("Jugador O ha ganado");
-                                                      }
-                                                      	else
-                                                          	if (tablero[1] == 'O' && tablero[4] == 'O' && tablero[7] == 'O') {  // segunda columna
-                                                              System.out.println("Jugador O ha ganado");
-                                                          }else
-                                                          	if (tablero[2] == 'O' && tablero[5] == 'O' && tablero[8] == 'O') {  // ultima columna 
-                                                                  System.out.println("Jugador O ha ganado");
-                                                              }else
-                                                              	if (tablero[0] == 'O' && tablero[4] == 'O' && tablero[8] == 'O') {  // diagonal desde la prim
-                                                                      System.out.println("Jugador O ha ganado");
-                                                                  }else
-                                                                  	if (tablero[2] == 'O' && tablero[4] == 'O' && tablero[6] == 'O') {
-                                                                          System.out.println("Jugador O ha ganado");
-                                                                      }
-                    
-                                                               	
-                                                	}
-            	                                  contador += 1;
-                 								}
-            
-                           
-                    
-                    
-                  
-        		
-        		
-        	}else if(opcion == 2) {
-        		
-        		System.out.println("todavia nada ");
-        	}
-        	
-                    	   
-                	   
-                
-                
-                
-                
-                
-                
-              
-                
-                 
-              
-                
-                	 
+                        } while (posicion < 0 || posicion > 8);
 
-                    // para asignar la posicion que asigno el usuario en el tablero 
+                        // Validar movimiento
+                        if (movimientoValido(posicion)) {
+                            if (jugador == 1) {
+                                mueveJugador1(posicion);
+                                iniciar();  // mostrar tablero actualizado
+                                jugador = 2;
+                            } else {
+                                mueveJugador2(posicion);
+                                iniciar();  // mostrar tablero actualizado
+                                jugador = 1;
+                            }
+                        } else {
+                            System.out.println("Movimiento inválido, casilla ocupada");
+                            if (jugador == 1) erroresJugador1++;
+                            else erroresJugador2++;
+                            continue; // volver a pedir posición
+                        }
 
-                
-                	
-                
-                    
-                
-                
-                //cambiamos el turno
-                
-                
-                
-        
-   
-            
-            
-                
-        	}while(true);
-        
-        System.out.println("partida terminada ");
-          
-            
-        }
-        
+                        contador++;
+
+                        // Comprobar ganador
+                        if (ganaJugador1()) {
+                            System.out.println("Jugador X ha ganado");
+                            ganadasJugador1++;
+                            break;
+                        } else if (ganaJugador2()) {
+                            System.out.println("Jugador O ha ganado");
+                            ganadasJugador2++;
+                            break;
+                        }
+
+                        // Comprobar empate
+                        if (contador == 9) {
+                            System.out.println("¡Empate!");
+                            empates++;
+                            break;
+                        }
+                    }
+
+                    dibujaTablero(); // limpiar tablero para nueva partida
+                    repetir = false;
+                }
+
+            } else if (opcion == 2) {
+                System.out.println("Victorias Jugador X: " + ganadasJugador1);
+                System.out.println("Victorias Jugador O: " + ganadasJugador2);
+                System.out.println("Errores Jugador X: " + erroresJugador1);
+                System.out.println("Errores Jugador O: " + erroresJugador2);
+                System.out.println("Empates: " + empates);
+            }
+
+        } while (true);
+
+        System.out.println("Partida terminada");
     }
 
+    public static void mostrartable() {
+        System.out.println("\n1. Jugar una partida");
+        System.out.println("2. Mostrar estadísticas");
+        System.out.println("3. Salir");
+        System.out.println("Seleccione una opción");
+        System.out.println("-----------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-----------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-----------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-----------");
+    }
 
+    public static void iniciar() {
+        System.out.println();
+        for (int i = 0; i < 9; i++) {
+            System.out.print("| " + tablero[i] + " ");
+            if ((i + 1) % 3 == 0) {
+                System.out.println("|");
+                System.out.println("-------------");
+            }
+        }
+        System.out.println();
+    }
 
+    public static void dibujaTablero() {
+        for (int i = 0; i < tablero.length; i++) {
+            tablero[i] = ' '; // tablero vacío
+        }
+    }
 
+    public static void mueveJugador1(int pos) {
+        tablero[pos] = 'X';
+    }
+
+    public static void mueveJugador2(int pos) {
+        tablero[pos] = 'O';
+    }
+
+    public static boolean movimientoValido(int pos) {
+        return pos >= 0 && pos <= 8 && tablero[pos] == ' ';
+    }
+
+    public static boolean ganaJugador1() {
+        return (tablero[0] == 'X' && tablero[1] == 'X' && tablero[2] == 'X') ||
+               (tablero[3] == 'X' && tablero[4] == 'X' && tablero[5] == 'X') ||
+               (tablero[6] == 'X' && tablero[7] == 'X' && tablero[8] == 'X') ||
+               (tablero[0] == 'X' && tablero[3] == 'X' && tablero[6] == 'X') ||
+               (tablero[1] == 'X' && tablero[4] == 'X' && tablero[7] == 'X') ||
+               (tablero[2] == 'X' && tablero[5] == 'X' && tablero[8] == 'X') ||
+               (tablero[0] == 'X' && tablero[4] == 'X' && tablero[8] == 'X') ||
+               (tablero[2] == 'X' && tablero[4] == 'X' && tablero[6] == 'X');
+    }
+
+    public static boolean ganaJugador2() {
+        return (tablero[0] == 'O' && tablero[1] == 'O' && tablero[2] == 'O') ||
+               (tablero[3] == 'O' && tablero[4] == 'O' && tablero[5] == 'O') ||
+               (tablero[6] == 'O' && tablero[7] == 'O' && tablero[8] == 'O') ||
+               (tablero[0] == 'O' && tablero[3] == 'O' && tablero[6] == 'O') ||
+               (tablero[1] == 'O' && tablero[4] == 'O' && tablero[7] == 'O') ||
+               (tablero[2] == 'O' && tablero[5] == 'O' && tablero[8] == 'O') ||
+               (tablero[0] == 'O' && tablero[4] == 'O' && tablero[8] == 'O') ||
+               (tablero[2] == 'O' && tablero[4] == 'O' && tablero[6] == 'O');
+    }
+}
