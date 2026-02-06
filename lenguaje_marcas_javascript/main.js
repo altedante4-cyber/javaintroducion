2d09555dd2f2cd60cf52ab7c653db9238ceb4d37
altedante4-cyber <altedante4@gmail.com>
2026-02-06 11:43:00 +0100

    subir cambios

---
+++ lenguaje_marcas_javascript/main.js
@@ -0,0 +1,33 @@
+document.writeln("pATAT<br>")
+document.write("Boniato")
+
+// codififco mis funciones chachis
+
+function saludo(nombre){
+     
+    return "Hola" + nombre + "!";
+}
+
+//var  y let
+
+let virginia  = saludo("Virginia");
+
+
+for(let index = 0; index < virginia.length ; index++ ){
+
+    document.writeln(virginia[index] + "<br>");
+    
+};
+
+
+for(const letra in virginia){
+     document.writeln(virginia[letra] + " ");
+}
+
+document.writeln(1+"5"+"<br>")
+document.writeln(1+5+"<br>")
+document.writeln(saludo("Felix"));
+
+document.writeln(111 == "111"); // esto devuelve true por que no es un comparador estrictor
+document.writeln(111 === "111") ; // esto devuelv false por que es un comparador estricto 
+
